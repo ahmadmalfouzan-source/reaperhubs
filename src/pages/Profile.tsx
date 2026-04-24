22df9fe1b64da6e0792f2bff4d16c5373c81e728
ahmadmalfouzan-source <ahmad.m.alfouzan@gmail.com>
2026-04-25 02:39:03 +0300

    Fix Profile.tsx import: change getLibrary to getLibraryItems

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { 
   getProfileByUsername, 
   getUserAchievements, 
-  getLibrary, 
+  getLibraryItems, 
   getCurrentUser, 
   updateProfile,
   followUser,
